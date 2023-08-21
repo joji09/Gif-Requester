22792a9f072ed3ecb37c99182cd2bd5218e1d20c
Juany Santana <juaaany@gmail.com>
2023-08-21 03:08:12 -0400

    Update app.js, missing remove button to remove added gifs

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,3 +1,34 @@
-const response = axios.get("http://api.giphy.com/v1/gifs/search");
 
-console.log("Let's get this party started!");
+// Giphy Token: KdOCM5UkYTUKhWuBqr9rbII6bYE1jPFt
+
+const gifArea = document.getElementById("gif-area");
+const searchInput = document.getElementById("search");
+const searchForm = document.getElementById("search-form");
+
+function addGif(data){
+    const numResults = data.length;
+    if (numResults){
+        const randomId = Math.floor(Math.random() * numResults);
+        const newDiv = document.createElement("div");
+        const newGif = document.createElement("img");
+        newGif.src = data[randomId].images.original.url
+        newDiv.appendChild(newGif);
+        gifArea.appendChild(newDiv);
+    }
+}
+
+searchForm.addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const searchKeyword = searchInput.value;
+    searchInput.value = "";
+
+    const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+        params: {
+            q: searchKeyword,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+
+    addGif(response.data.data)
+})
